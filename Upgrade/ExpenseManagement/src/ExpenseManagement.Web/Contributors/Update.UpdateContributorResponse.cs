d32754eeabda77770bf0808ea00461b599187640
Toky <72009671+herostoky@users.noreply.github.com>
2024-11-27 10:19:13 +0300

    feat: Initial commit for upgrade
      - Create files for dotnet 9 solution
      - Use template from Ardalis.CleanArchitecture.Template

---
+++ Upgrade/ExpenseManagement/src/ExpenseManagement.Web/Contributors/Update.UpdateContributorResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace ExpenseManagement.Web.Contributors;
+
+public class UpdateContributorResponse(ContributorRecord contributor)
+{
+  public ContributorRecord Contributor { get; set; } = contributor;
+}
---
+++ Upgrade/ExpenseManagement/tests/ExpenseManagement.UnitTests/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿global using System.Runtime.CompilerServices;
+global using Ardalis.SharedKernel;
+global using ExpenseManagement.Core.ContributorAggregate;
+global using ExpenseManagement.UseCases.Contributors.Create;
+global using FluentAssertions;
+global using MediatR;
+global using Microsoft.Extensions.Logging;
+global using NSubstitute;
+global using Xunit;
