b429e39cd02f2f318780486ceabf138db1a52842
Toky <72009671+herostoky@users.noreply.github.com>
2024-12-18 14:35:15 +0300

    fix: Add API project to .NET Aspire (edit)

--- src/ExpenseManagement.AspireHost/Program.cs
+++ src/ExpenseManagement.AspireHost/Program.cs
@@ -1,5 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.ExpenseManagement_Web>("web");
+builder.AddProject<Projects.ExpenseManagement_Api>("api");
 
 builder.Build().Run();
