125ddb561148b7cbdf0956482f3abe3b98d11a0e
Toky <72009671+herostoky@users.noreply.github.com>
2024-12-20 15:39:50 +0300

    feat: UserController creation

---
+++ src/ExpenseManagement.Api/Constants/Routes.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpenseManagement.Api.Constants;
+
+public static class Routes
+{
+  public static class User
+  {
+    public const string SignUp = "signup";
+    public const string SignIn = "signin";
+  }
+}
---
+++ src/ExpenseManagement.Api/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using ExpenseManagement.Api.Constants;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseManagement.Api.Controllers;
+
+
+[ApiController]
+[Route($"api/{nameof(Routes.User)}")]
+public class UserController(ILogger<UserController> logger) : ControllerBase
+{
+  [HttpPost(template: Routes.User.SignUp, Name = nameof(Routes.User.SignUp))]
+  public async Task<IActionResult> UserSignUp()
+  {
+    logger.LogInformation("Sign up User");
+    await Task.Delay(millisecondsDelay: 1000);
+    return Ok();
+  }
+
+  /*[HttpPost(template: Routes.User.SignUp, Name = nameof(Routes.User.SignUp))]
+  public BaseResponseDto<UserSignUpResponseDto> UserSignUp(UserSignUpRequestDto userSignUpRequestDto)
+  {
+    logger.LogDebug(message: "Trying to sign up user : {User}", userSignUpRequestDto);
+    var responseDto = userService.UserSignUp(userEmail: userSignUpRequestDto.UserEmail.Trim(),
+        userFullName: userSignUpRequestDto.UserFullName.Trim(), userPassword: userSignUpRequestDto.UserPassword);
+    var httpCode = Convert.ToInt32(HttpStatusCode.Created);
+    return new BaseResponseDto<UserSignUpResponseDto>
+    {
+      HttpStatusCode = httpCode,
+      IsError = false,
+      ResponseMessage = "User signed up",
+      Body = responseDto
+    };
+  }
+
+  [HttpPost(template: Routes.User.SignIn, Name = nameof(Routes.User.SignIn))]
+  public BaseResponseDto<UserSignInResponseDto> UserSignIn(UserSignInRequestDto userSignInRequestDto)
+  {
+    logger.LogDebug(message: "Trying to sign in user : {User}", userSignInRequestDto);
+    var responseDto = userService.UserSignIn(userEmail: userSignInRequestDto.UserEmail.Trim(), userPassword: userSignInRequestDto.UserPassword);
+    var httpCode = Convert.ToInt32(HttpStatusCode.Accepted);
+    return new BaseResponseDto<UserSignInResponseDto>
+    {
+      HttpStatusCode = httpCode,
+      IsError = false,
+      ResponseMessage = "User signed in",
+      Body = responseDto
+    };
+  }*/
+
+}
